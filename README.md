66b5cfe387d6f7cf1fc16ca616ce66b27c541747
RE000 <re.paterson000@gmail.com>
2020-07-09 11:02:41 +1000

    test 1

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# HW9_MU_RPaterson_ReadMeGenerator
-Good ReadME Generator utilising Node.JS
+# Good README Generator
+[![License](https://img.shields.io/badge/License-None%20-lightgrey.svg)]()
+## Description
+The aim of this project was to create a README generator utilizing Node.js and ES6+. This is a command line application that uses user input along with the NPM inquirer package.
+
+## Table of Contents
+- [Description](#Description)
+- [Installation Instructions](#Installation-Instructions)
+- [Usage Information](#Usage-Information)
+- [Contribution Guidelines](#Contribution-Guidelines)
+- [Test Instructions](#Test-Instructions)
+- [Licence](#Licence)
+- [Questions](#Questions)
+
+## Installation Instructions
+1. Create an index.js file in your local folder
+2. Ensure you have Node.js installed, along with NPM
+3. Generate a package.json file
+4. Install inquirer
+5. Copy the contents of this index.js file into your index.js file. 
+6. Copy the license.txt file you want to use for your own project into your project folder. 
+7. Note: If you want to use a different license than listed. Make sure to add a .txt file for that license and update the links in the index.js file to ensure correct functionality.
+
+## Usage Information
+1. Using the command line, navigate your way to the folder containing your index.js file.
+2. Enter 'node index.js' into your command line.
+3. Follow the input instructions.
+4. Open the COPYME.md file created and copy the contents into the README.md file for you project.
+5. All done!
+
+## Contribution Guidelines
+none
+
+## Test Instructions
+none
+
+## License
+This application is not using a license
+
+## Questions
+For further information, please feel free to contact me on either of the links below
+
+GitHub: https://github.com/re093
+ Email: re.paterson000@gmail.com
---
+++ index.js
@@ -0,0 +1,124 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+inquirer.prompt([
+  {
+    type: "input",
+    name: "title",
+    message: "Enter the name of your project"
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Enter a description for your project"
+  },
+  {
+    type: "input",
+    message: "Enter your installation Instructions",
+    name: "installation",
+  },
+  {
+    type: "input",
+    message: "Enter your usage information",
+    name: "usage",
+  },
+  {
+    type: "input",
+    message: "Enter the Contribution Guidelines",
+    name: "contributing",
+  },
+  {
+    type: "input",
+    message: "Enter Test Instructions",
+    name: "tests",
+  },
+  {
+      type: "list",
+      name: "license",
+      choices: [
+          "MIT License",
+          "ISC License",
+          "GNU General Public License v3.0",
+          "Apache License v2.0",
+          "None"
+      ]
+  },
+  {
+      type: "input",
+      message: "Enter your GitHub Username",
+      name: "github"
+  },
+  {
+      type: "input",
+      message: "Enter your Email Address",
+      name: "email"
+  }
+
+]).then(function(data) {
+
+    let licenseUsed = "";
+    let licenseButton = "";
+
+    if (data.license === "MIT License") {
+        console.log("MIT License used!");
+        licenseUsed = "This application is license using the MIT License [Link Here](MIT-license.txt)";
+        licenseButton = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://choosealicense.com/licenses/mit/)";
+    };
+
+    if (data.license === "ISC License") {
+        console.log("ISC License used!");
+        licenseUsed = "This application is license using the ISC License [Link Here](ISC-license.txt)";
+        licenseButton = "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://choosealicense.com/licenses/isc/)";
+    };
+
+    if (data.license === "GNU General Public License v3.0") {
+        console.log("GNU Public License used!");
+        licenseUsed = "This application is license using the GNU General Public License [Link Here](GNU-license.txt)";
+        licenseButton = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-red.svg)](https://choosealicense.com/licenses/gpl-3.0/)";
+    };
+
+    if (data.license === "Apache License v2.0") {
+        console.log("Apache Licence used!");
+        licenseUsed = "This application is license using the Apache License v2.0 [Link Here](Apache-license.txt)";
+        licenseButton = "[![License](https://img.shields.io/badge/License-Apache%202.0-purple.svg)](https://choosealicense.com/licenses/apache-2.0/)";
+    }
+    if (data.license === "None") {
+        console.log("You have selected no license!");
+        licenseUsed = "This application is not using a license"
+        licenseButton = "[![License](https://img.shields.io/badge/License-None%20-lightgrey.svg)]()";
+    }
+
+    fs.writeFile(
+      "COPYME.md",
+      `# ${data.title}
+      ${licenseButton}
+      ## Description
+      ${data.description}
+      ## Table of Contents
+      - [Description](#Description)
+      - [Installation Instructions](#Installation-Instructions)
+      - [Usage Information](#Usage-Information)
+      - [Contribution Guidelines](#Contribution-Guidelines)
+      - [Test Instructions](#Test-Instructions)
+      - [Licence](#Licence)
+      - [Questions](#Questions)
+      ## Installation Instructions
+      ${data.installation}
+      ## Usage Information
+      ${data.usage}
+      ## Contribution Guidelines
+      ${data.contributing}
+      ## Test Instructions
+      ${data.tests}
+      ## License
+      ${licenseUsed}
+      ## Questions
+      For further information, please feel free to contact me on either of the links below
+      GitHub: https://github.com/${data.github}
+      Email: ${data.email}`
+      ,function(){
+          console.log("Success! COPYME.md Generated!") 
+  });
+});
+
+// Badges sourced from shields.io/category/license
